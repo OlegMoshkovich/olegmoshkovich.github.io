9529b4bc68bd07cd20c2293cb506300e6d6b734a
OlegMoshkovich <oleg.mosh@gmail.com>
2024-04-17 22:08:37 +0200

    add the theme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ThemeProvider } from '@mui/material/styles';
+import { lightTheme, darkTheme} from './theme'
+import useStore from './Store';
+
+
+
+
+const Sample = () =>{
+const [dark, setDark] = useState(false)
+const { colorTheme} = useStore();
+
+  return(
+    <React.StrictMode>
+        <ThemeProvider theme={dark ? darkTheme(colorTheme) : lightTheme(colorTheme)}>
+          <App changeTheme={() => setDark(!dark)} darkTheme={dark}/>
+        </ThemeProvider>
+      </React.StrictMode>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<Sample/>);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
