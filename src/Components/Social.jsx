e8fa0f00c4314bf15d67e5616c7b45f432e1c4de
OlegMoshkovich <oleg.mosh@gmail.com>
2024-08-21 22:00:58 +0200

    fix resume

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -35,7 +35,7 @@ const Social = () => {
                   <ImageListItem key={'image'} >
                     <img
                       src={`${process.env.PUBLIC_URL}/images/resume.png`}
-                      alt={'image_hero'}
+                      alt={'resume'}
                       loading="lazy"
                     />
                   </ImageListItem>
