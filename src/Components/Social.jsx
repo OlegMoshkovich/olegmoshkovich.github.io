1d86acf9e5d590e7cc8be71e1aaa14890b1fd9c5
OlegMoshkovich <oleg.mosh@gmail.com>
2024-09-01 13:41:02 +0200

    add qr code for sharing

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -9,6 +9,7 @@ import Link from '@mui/material/Link'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import XIcon from '@mui/icons-material/X';
+import QrCodeIcon from '@mui/icons-material/QrCode';
 import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
 import Dialog from './Dialog';
 
@@ -21,6 +22,7 @@ const Social = () => {
     spacing={0}
     sx={{marginRight:'-5px'}}
     >
+
       <IconButton size='small'
           onClick={()=>window.open('https://x.com/OlegMoshkovich')}
           sx={{border:'none'}}>
@@ -36,6 +38,29 @@ const Social = () => {
           sx={{border:'none'}}>
         <GitHubIcon fontSize='small'/>
       </IconButton>
+      <Dialog
+        actionTitle={'OK'}
+        icon={<QrCodeIcon fontSize='small'/>}
+        dialogTitle={
+          <Typography variant='body2'>
+            Oleg Moshkovich
+          </Typography>
+        }
+        dialogContent={
+          <Box>
+          <ImageList cols={1}>
+            <ImageListItem key={'image'}>
+              <img
+                src={`${process.env.PUBLIC_URL}/images/11.png`}
+                alt={'resume'}
+                loading="lazy"
+                style={{ width: '200px', height: '200px' }}  // Set width and height here
+              />
+            </ImageListItem>
+          </ImageList>
+        </Box>
+        }
+      />
     </Stack>
   )
 
