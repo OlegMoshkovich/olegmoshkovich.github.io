59d89e97a06cd49120c1c36f178762ddf8390de7
OlegMoshkovich <oleg.mosh@gmail.com>
2024-09-02 10:59:17 +0200

    slight improvement

--- src/Components/Description.jsx
+++ src/Components/Description.jsx
@@ -21,26 +21,26 @@ function Description() {
         }}
         >
           <Stack justifyContent={'center'}  spacing={1} sx={{padding:'30px 10px', width:isMobile ? 320 : 500}}>
-            <Typography  variant='body2' sx={{fontWeight:'bold'}}>
-              Hello.
-            </Typography>
-            <Typography  variant='body2' sx={{fontWeight:'bold'}}>
-              I am a product architect and product engineer with BIM experetise working at the intersection of AEC and technology.
-            </Typography>
-            <Typography  variant='body2'>
-              I have worked in design and engineering offices, construction sites, and startups.
-              These experiences have equipped me with the ability to adapt to different organizational structures and cultures,
-              allowing me to contribute effectively in both engineering and product roles.
-            </Typography>
-            <Typography  variant='body2'>
-              I am convinced that open-standards and open-source software are shaping the technological foundation of the building industry.
-            </Typography>
-            <Typography variant='body2'>
-              Most recently, I co-founded Bldrs.ai, a company focused on introducing tech tools into AEC workflows.
-            </Typography>
-            <Typography variant='body2'>
-              I hope to use my knowledge and experience to contribute to the Rebuild Ukraine effort.
-            </Typography>
+          <Typography variant='body2' sx={{ fontWeight: 'bold' }}>
+            Hello.
+          </Typography>
+          <Typography variant='body2' sx={{ fontWeight: 'bold' }}>
+            I am a product manager and product engineer with BIM expertise, working at the intersection of AEC and technology.
+          </Typography>
+          <Typography variant='body2'>
+            I have worked in design and engineering offices, on construction sites, and in startups.
+            These experiences have equipped me with the ability to adapt to different organizational structures and cultures,
+            allowing me to contribute effectively in both engineering and product roles.
+          </Typography>
+          <Typography variant='body2'>
+            I am convinced that open standards and open-source software are shaping the technological foundation of the building industry.
+          </Typography>
+          <Typography variant='body2'>
+            Most recently, I co-founded Bldrs.ai, a company focused on introducing tech tools into AEC workflows.
+          </Typography>
+          <Typography variant='body2'>
+            I hope to use my knowledge and experience to contribute to the Rebuild Ukraine effort.
+          </Typography>
           </Stack>
         </Paper>
   );
