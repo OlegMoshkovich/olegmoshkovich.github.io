4d65fba8ed52ad3f02dbbec53cd72933fd2683c6
OlegMoshkovich <oleg.mosh@gmail.com>
2024-09-10 13:02:18 +0200

    add the link to craftworkers

--- src/AppBar/LeftSide.jsx
+++ src/AppBar/LeftSide.jsx
@@ -3,8 +3,6 @@ import {
   Stack,
 } from '@mui/material';
 
-
-
 const LeftSide = ({onGoToLocation}) => {
   return(
     <Stack
@@ -12,12 +10,14 @@ const LeftSide = ({onGoToLocation}) => {
     alignItems="center"
     spacing={1}
     >
+      <a href="https://www.youtube.com/channel/UCK0LS82kZMwKIRTH4ILa8nA" target="_blank" rel="noopener noreferrer">
         <img
           src={`${process.env.PUBLIC_URL}/images/12.png`}
           alt={'image_hero'}
           loading="lazy"
           style={{width:50}}
         />
+      </a>
     </Stack>
     )
   }
